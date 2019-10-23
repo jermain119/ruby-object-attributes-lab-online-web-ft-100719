07cd33b342445871b7bc78eba12110577d91acf2
joshua richards <jermain119@gmail.com>
2019-10-23 01:21:05 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,10 @@
 class Person
   
+<<<<<<< HEAD
   def name=(name)
+=======
+  def initialize(name)
+>>>>>>> 4c2286af2d098b354ffb39a4fc56fa53850c9984
     @name = name
   end
   
@@ -8,7 +12,11 @@ class Person
     @name
   end
 
+<<<<<<< HEAD
   def job=(job)
+=======
+  def initialize(job)
+>>>>>>> 4c2286af2d098b354ffb39a4fc56fa53850c9984
     @job = job
   end 
 
